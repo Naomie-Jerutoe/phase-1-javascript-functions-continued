668d5eed5477fc45306070c8958f72575ce1001f
Naomi Lagat <naomiejerutoe@gmail.com>
2023-12-12 12:44:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun( activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+};
+mondayWork();
+
+function wrapAdjective(string = "*"){
+    const innerFunction = function(adj = "special"){
+        return `You are ${string}${adj}${string}!`;
+    };
+        return innerFunction;
+}
